c4e305b7688e6a1029657f1dcdd99547ce7cff71
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-02 13:32:53 +0200

    Initial commit

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
+ 
---
+++ api/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public required string UserName { get; set; }
+
+    }
+}
