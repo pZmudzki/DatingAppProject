175bd6c2812269b9193c471a5191777df7aeb1d4
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-05 13:50:23 +0200

    Create Login and User DTOs

---
+++ api/Models/DTO/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models.DTO
+{
+    public class LoginDto
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
