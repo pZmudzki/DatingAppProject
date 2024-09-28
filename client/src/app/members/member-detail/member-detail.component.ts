e74b389d425604070f974a6e4ee3ecb222849205
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-28 15:48:33 +0200

    Implement routing

---
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-detail.component.html',
+})
+export class MemberDetailComponent {}
---
+++ client/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-list.component.html',
+})
+export class MemberListComponent {}
