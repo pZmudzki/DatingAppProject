28c7b410d1ab1bdee2f5df00aa284d6259cb66c9
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-03 19:49:21 +0200

    Add AutoMapper service to map attributes to DTOs

---
+++ api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using api.Models.DTO;
+using AutoMapper;
+
+namespace api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>();
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
