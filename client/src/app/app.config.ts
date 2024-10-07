9076894551c9db299325e755a07de641ad72836f
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-07 21:36:44 +0200

    Add jwt interceptor middleware to improve adding header bearer token

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -7,12 +7,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { errorInterceptor } from './_interceptors/error.interceptor';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right',
