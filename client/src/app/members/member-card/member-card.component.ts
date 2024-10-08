f449a4a1f0a652c64ddd97d771ec41ae9c2f0c7b
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-08 19:48:06 +0200

    Implement member detail view and connect it to member card

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { Member } from '../../_models/member';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './member-card.component.html',
 })
 export class MemberCardComponent {
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,4 +9,20 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './member-detail.component.html',
 })
-export class MemberDetailComponent {}
+export class MemberDetailComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: (member) => (this.member = member),
+    });
+  }
+}
