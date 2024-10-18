fb03115486ff662e9063be599509546e0eab3266
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-18 22:38:19 +0200

    Implement photo deletion functionality

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -29,17 +29,19 @@ export class PhotoEditorComponent implements OnInit {
   fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
-  // deletePhoto(photo: Photo) {
-  //   this.memberService.deletePhoto(photo).subscribe({
-  //     next: (_) => {
-  //       const updatedMember = { ...this.member() };
-  //       updatedMember.photos = updatedMember.photos.filter(
-  //         (x) => x.id !== photo.id
-  //       );
-  //       this.memberChange.emit(updatedMember);
-  //     },
-  //   });
-  // }
+
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo).subscribe({
+      next: (_) => {
+        const updatedMember = { ...this.member() };
+        updatedMember.photos = updatedMember.photos.filter(
+          (x) => x.id !== photo.id
+        );
+        this.memberChange.emit(updatedMember);
+      },
+    });
+  }
+
   setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: (_) => {
