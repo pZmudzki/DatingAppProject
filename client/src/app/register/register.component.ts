5a96a492e44c9ff4302ac5dc859dea30f3ab341f
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-20 14:36:13 +0200

    Add register form validation on client side

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,25 +1,62 @@
-import { Component, inject, output } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { TextInputComponent } from '../_forms/text-input/text-input.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TextInputComponent],
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private toast = inject(ToastrService);
   private accountService = inject(AccountService);
+  registerForm: FormGroup = new FormGroup({});
   cancelRegister = output<boolean>();
+  validationErrors: string[] | undefined;
 
   model: any = {};
 
-  registerForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(16),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () =>
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { isMatching: true };
+    };
+  }
 
   register() {
     this.accountService.register(this.registerForm.value).subscribe({
