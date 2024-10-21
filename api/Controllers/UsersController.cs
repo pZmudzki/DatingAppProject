2a116d6a9baa09331629e9cc9fc27e4437090032
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-21 22:34:39 +0200

    Fix adding first photo to be automatically set to main photo

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -68,6 +68,8 @@ namespace api.Controllers
                 PublicId = result.PublicId,
             };
 
+            if(user.Photos.Count == 0) photo.IsMain = true; 
+
             user.Photos.Add(photo);
 
             if (await userRepository.SaveAllAsync()) 
