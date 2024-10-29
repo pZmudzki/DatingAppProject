dd141d762b2765b6c7dd13638888dcbc65ea424e
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-10-29 19:48:39 +0100

    Implement pagination on client side

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
 import { Photo } from '../_models/photo';
+import { PaginatedResult } from '../_models/pagination';
 
 @Injectable({
   providedIn: 'root',
@@ -12,64 +13,80 @@ export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
 
-  members = signal<Member[]>([]);
-  member = signal<Member | null>(null);
+  // members = signal<Member[]>([]);
+  // member = signal<Member | null>(null);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
 
-  getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
-      next: (members) => this.members.set(members),
-    });
+  getMembers(pageNumber?: number, pageSize?: number) {
+    let params = new HttpParams();
+    if (pageNumber && pageSize) {
+      params = params.append('pageNumber', pageNumber);
+      params = params.append('pageSize', pageSize);
+    }
+
+    return this.http
+      .get<Member[]>(this.baseUrl + 'users', { observe: 'response', params })
+      .subscribe({
+        next: (response) => {
+          this.paginatedResult.set({
+            items: response.body as Member[],
+            pagination: JSON.parse(response.headers.get('Pagination')!),
+          });
+        },
+      });
   }
 
   getMember(username: string) {
-    const member = this.members().find((x) => x.userName === username);
+    // const member = this.members().find((x) => x.userName === username);
 
-    if (member !== undefined) return of(member);
+    // if (member !== undefined) return of(member);
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      tap(() => {
-        this.members.update((members) =>
-          members.map((m) => (m.userName === member.userName ? member : m))
-        );
-      })
-    );
+    return this.http
+      .put(this.baseUrl + 'users', member)
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => (m.userName === member.userName ? member : m))
+      //   );
+      // })
+      ();
   }
 
   setMainPhoto(photo: Photo) {
     return this.http
       .put(this.baseUrl + 'users/set-main-photo/' + photo.id, {})
-      .pipe(
-        tap(() => {
-          this.members.update((members) =>
-            members.map((m) => {
-              if (m.photos.includes(photo)) {
-                m.photoUrl = photo.url;
-              }
-              return m;
-            })
-          );
-        })
-      );
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => {
+      //       if (m.photos.includes(photo)) {
+      //         m.photoUrl = photo.url;
+      //       }
+      //       return m;
+      //     })
+      //   );
+      // })
+      ();
   }
 
   deletePhoto(photo: Photo) {
     return this.http
       .delete(this.baseUrl + 'users/delete-photo/' + photo.id)
-      .pipe(
-        tap(() => {
-          this.members.update((members) =>
-            members.map((m) => {
-              if (m.photos.includes(photo)) {
-                m.photos = m.photos.filter((x) => x.id !== photo.id);
-              }
-              return m;
-            })
-          );
-        })
-      );
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => {
+      //       if (m.photos.includes(photo)) {
+      //         m.photos = m.photos.filter((x) => x.id !== photo.id);
+      //       }
+      //       return m;
+      //     })
+      //   );
+      // })
+      ();
   }
 }
